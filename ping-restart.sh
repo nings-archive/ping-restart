a393420a53ccbd3a64db8352b6e11b4ad6678b5f
ningSP4 <ningyuan.sg@gmail.com>
2017-01-14 20:59:11 +0800

    Fix syntax error: missing ' '

--- ping-restart.sh
+++ ping-restart.sh
@@ -2,7 +2,7 @@
 
 ping google.com -c 1  # ping--no packets exit 1; good exit 0
 
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
 	echo "[`date`] EXIT CODE $?, restarting..." >> ping-restart.log
 	reboot
 fi
