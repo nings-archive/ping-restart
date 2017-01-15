32e43c92a0dfc7358e2bbb9518b99344715cf27b
ningSP4 <ningyuan.sg@gmail.com>
2017-01-15 11:19:08 +0800

    Fix typo with var assignment @ ln4 '$code=$?' -> 'code=$?'

--- ping-restart.sh
+++ ping-restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ping google.com -c 1  # ping--no packets exit 1; good exit 0
-$code=$?
+code=$?
 
 if [ "${code:0}" != "0" ]; then
 	echo "[`date`] EXIT CODE $?, restarting..." >> ping-restart.log
