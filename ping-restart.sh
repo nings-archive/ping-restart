931f549a83aae2e04e039efed2f9044e97df27ec
ningSP4 <ningyuan.sg@gmail.com>
2017-01-16 12:58:43 +0800

    Change format of logging (including add $(uptime -p))
    Now prints in the format,
    
    DD MON HH:MM:SS AM | up XX minutes | Exit n. Restarting...
    
    e.g.,
    
    16 Jan 13:00:00 PM | up 42 minutes | Exit 1. Restarting...

--- ping-restart.sh
+++ ping-restart.sh
@@ -4,8 +4,8 @@ ping google.com -c 1  # ping--no packets exit 1; good exit 0
 code=$?
 
 if [ "${code:0}" != "0" ]; then
-	echo "[`date`] EXIT CODE $code, restarting..." >> "$(pwd)/ping-restart.log"
+	echo "$(date +"%d %b %r") | $(uptime -p) | Exit ${code}. Restarting..." >> "$(pwd)/ping-restart.log"
 	reboot
 #else
-#      	echo "[`date`] EXIT CODE $code, OK" >> "$(pwd)/ping-restart.log"
+#      	echo "$(date) EXIT CODE $code, OK" >> "$(pwd)/ping-restart.log"
 fi
