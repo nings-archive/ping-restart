e5370b7e612cb03aa9f4cf39351d23f182770c3d
ning <ningyuan.sg@gmail.com>
2017-01-19 15:48:11 +0800

    Change reboot -> shutdown -r now
    Got command not found error (reflected in cron.log) for command
    reboot, stackoverflow recommends shutdown -r now instead.
    
    http://stackoverflow.com/questions/27452790/bash-reboot-command-not-found

--- ping-restart.sh
+++ ping-restart.sh
@@ -5,7 +5,7 @@ code=$?
 
 if [ "${code:0}" != "0" ]; then
 	echo "$(date +"%d %b %r") | $(uptime -p) | Exit ${code}. Restarting..." >> "$(pwd)/ping-restart.log"
-	reboot
+	shutdown -r now
 #else
 #      	echo "$(date) EXIT CODE $code, OK" >> "$(pwd)/ping-restart.log"
 fi
