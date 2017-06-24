a7dd067041f6fd9eb84a96b9c664a7438313a970
ning <ningyuan.sg@gmail.com>
2017-06-24 09:18:03 +0800

    Fix reboot not working by using full path to reboot
    Don't know why it won't work, though

--- ping-restart.sh
+++ ping-restart.sh
@@ -5,7 +5,7 @@ time_stamp=$(date +"%Y%m%d-%H%M")
 
 if [ $exit_code -ne 0 ]; then 
     (1>&2 echo "$time_stamp: failed with exit code $exit_code; restarting now")
-    reboot
+    /sbin/reboot
 else
     echo "$time_stamp: ok"
 fi
